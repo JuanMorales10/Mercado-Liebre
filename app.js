fc9d61a0d6b2e001c1a29819a93286b4d2f78206
JMorDev <juanmoralesp19@gmail.com>
2023-07-06 22:58:26 -0300

    Posicionamiento Web

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const ruta = path.resolve(__dirname,'./public')
+
+app.use(express.static(ruta))
+
+app.get('/',(req,res)=>{
+    // res.sendFile(path.resolve(__dirname,'./views/home.html'))
+    res.sendFile(path.join(__dirname,'./views/home.html'))
+})
+
+app.listen(3080,()=>{
+    console.log('Servidor andando en el puerto 3080')
+})
