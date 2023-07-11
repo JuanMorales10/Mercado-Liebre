470735c6ac672d7eb82fc12692943c580493c49f
JMorDev <juanmoralesp19@gmail.com>
2023-07-11 12:35:30 -0300

    Agrego rutas en app.js

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ app.get('/registroProducto',(req,res)=>{
     res.sendFile(path.join(__dirname,'./views/registroProducto.html'))
 })
 
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/register.html'))
+})
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/login.html'))
+})
 
 app.listen(3080,()=>{
     console.log('Servidor andando en el puerto 3080')
