53c88662c2a336afd0c679328142aa5733e0c812
JMorDev <juanmoralesp19@gmail.com>
2023-08-15 10:34:44 -0300

    Agrego Express Validator avanzado

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,25 +1,37 @@
 const productModel = require('../modals/productModel')
+const { validationResult } = require('express-validator');
+
 
 const controller = {
 
     getRegistroProducto: (req, res) => {
         res.render('registroProducto')
     },
-    getEditProduct:(req, res) => {
+    getEditProduct: (req, res) => {
         const productId = req.params.id;
         const selectedProduct = productModel.findById(productId);
-        res.render('editProduct', {product : selectedProduct})
+        res.render('editProduct', { product: selectedProduct })
     },
     createProduct: (req, res) => {
-        
-        const newProduct = {
-            nombre: req.body.nombre,
-            precio: req.body.precio,
-            categoria: req.body.categoria,
-            ciudad: req.body.ciudad,
-            image: req.file.filename,
-            descripcion: req.body.descripcion,
-            enOferta: false
+
+        const errors = validationResult(req);
+        console.log(req.body)
+
+        if (!errors.isEmpty()) {
+            return res.render('registroProducto', {
+                errors: errors.array(),
+                old: req.body
+            });
+        } else {
+            const newProduct = {
+                nombre: req.body.nombre,
+                precio: req.body.precio,
+                categoria: req.body.categoria,
+                ciudad: req.body.ciudad,
+                image: req.file.filename,
+                descripcion: req.body.descripcion,
+                enOferta: false
+            }
         }
 
         const createdProduct = productModel.createProduct(newProduct);
@@ -29,22 +41,22 @@ const controller = {
     editProduct: (req, res) => {
 
         let updatedProduct = {
-          id: Number(req.params.id),
+            id: Number(req.params.id),
         };
-    
+
         updatedProduct = {
-          ...updatedProduct,
-          ...req.body,
+            ...updatedProduct,
+            ...req.body,
         };
-    
+
         productModel.updateProduct(updatedProduct);
-    
+
         res.redirect('/');
     },
-    deleteProduct:(req, res) => {
+    deleteProduct: (req, res) => {
 
         productModel.deleteProduct(Number(req.params.id));
-    
+
         res.redirect("/")
     }
 
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const productController = require('../controllers/productController')
 const multer = require("multer");
 const path = require("path");
+const { body } = require('express-validator');
+
+const validarRegistro = [
+    body('nombre').notEmpty().withMessage('Debes Completar el Nombre'),
+    body('precio').notEmpty().withMessage('Debes completar el Precio'),
+    body('ciudad').notEmpty().withMessage('Debes Completar la Ciudad'),
+    body('image').notEmpty().withMessage('Debes ingresar una imagen'),
+    body('enOferta').notEmpty().withMessage('Debes Completar la Seccion: En Oferta'),
+    body('descripcion').notEmpty().withMessage('Debes Completar la Descripcion'),
+];
 
 
 const storage = multer.diskStorage({
@@ -24,7 +34,7 @@ router.get('/registroProducto', productController.getRegistroProducto);
 router.get('/editProduct/:id', productController.getEditProduct)
 
 //Post
-router.post('/registroProducto', upload.single('image') ,productController.createProduct)
+router.post('/registroProducto', upload.single('image'), validarRegistro ,productController.createProduct)
 
 //Put
 router.put('/editProduct/:id', upload.single('image') ,productController.editProduct)
